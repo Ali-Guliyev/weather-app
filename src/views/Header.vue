b90f7b11fd2d008bb5e4902b866ec43ebed33248
Ali Guliyev <ali.guliyevoo7@mail.com>
2021-03-09 20:53:44 +0300

    app updated

---
+++ src/views/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="header__wrapper">
+      <div class="column-1">
+        <h1 class="header__title">Weather App</h1>
+        <p class="header__p">Search current weather data wherever you want</p>
+      </div>
+      <div class="column-2">
+        <img class="header__img" src="@/assets/img/favicon.png" />
+      </div>
+    </div>
+
+    <div class="header__inputWrapper">
+      <input
+        class="header__searchInput"
+        spellcheck="false"
+        placeholder="Search..."
+      />
+    </div>
+  </header>
+</template>
---
+++ src/views/Main.vue
@@ -0,0 +1,23 @@
+<template> </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  setup() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const coords = position.coords;
+
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=b36ca9e34424b82fbc2276da7702e37f`
+          )
+          .then((res) => {
+            console.log(res.data);
+          });
+      });
+    }
+  },
+};
+</script>
