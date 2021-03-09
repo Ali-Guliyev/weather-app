1733d2c28774775f0a98dc3222696a7d975d1c31
Ali Guliyev <ali.guliyevoo7@mail.com>
2021-03-09 22:40:51 +0300

    app changed

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,10 +1,16 @@
-<template> </template>
+<template>
+  {{ weather.name }}
+  {{ weather.temp }}°C
+</template>
 
 <script>
 import axios from "axios";
+import { ref } from "vue";
 
 export default {
   setup() {
+    const weather = ref({});
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const coords = position.coords;
@@ -14,10 +20,14 @@ export default {
             `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=b36ca9e34424b82fbc2276da7702e37f`
           )
           .then((res) => {
-            console.log(res.data);
+            weather.value = res.data;
+            weather.value.temp = Math.round(weather.value.main.temp - 273.15);
+            console.log(weather.value);
           });
       });
     }
+
+    return { weather };
   },
 };
 </script>
