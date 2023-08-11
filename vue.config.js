55216f21c64fb59bbfbf023bc5582c7362772227
Ali Guliyev <54935778+Ali-Guliyev@users.noreply.github.com>
2023-08-11 15:31:35 +0300

    pwa fix

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      name: 'Weather App',
+      short_name: 'Weather App',
+      start_url: './',
+      display: 'standalone',
+      theme_color: '#78bff7',
+      msTileColor: '#6dade1',
+      icons: [
+        {
+          src: './src/img/icons/icon192x192.png',
+          sizes: '192x192',
+          type: 'image/svg+xml',
+          purpose: 'any maskable',
+        },
+        {
+          src: './src/img/icons/icon512x512.png',
+          sizes: '512x512',
+          type: 'image/svg+xml',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+  },
+};
